526f540cc5eb9988b10b4fa9d03f0063a1a374ea
Morteza Akbari <mortezaakbari@mortezas-air.students.netlogon.liu.se>
2022-11-23 13:15:04 +0100

    Fixat för fyra drönare

--- ros_ws/src/crazyswarm/scripts/Min_Follow_leader.py
+++ ros_ws/src/crazyswarm/scripts/Min_Follow_leader.py
@@ -1,8 +1,10 @@
+
 """Follow the leader"""
 #!/usr/bin/env
 from pycrazyswarm import Crazyswarm
 import numpy as np
 import uav_trajectory
+import math
 
 
 if __name__ == "__main__":
@@ -24,17 +26,41 @@ if __name__ == "__main__":
         
 
 
-        allcfs.takeoff(targetHeight=1.0, duration=2.0)
-        timeHelper.sleep(2.5)
+        cf1.takeoff(targetHeight=0.5, duration=2.0)
+        timeHelper.sleep(allcfs.takeoff.duration) # Här vill jag att cf1 sleepa så länge den inte har nått sitt traget.
+        """
         for cf in allcfs.crazyflies:
             pos = np.array(cf.initialPosition) + np.array([0, 0, 1.0])
             cf.goTo(pos, 0, 2.0)
         timeHelper.sleep(2.5)
-
+        """
+        cf1_pos = np.array([0,0,0.5],[0,0,0.5],[0,0,0.5])
+        nbrloops = 10*(traj1.duration * TIMESCALE +2)
+        n = 0
+        while n < nbrloops:
+            cf1_pos.append(cf1.position())
+            cf2_pos = cf2.position()
+            Dis = math.dist(cf2_pos,cf1_pos)
+            Safe_dist = 0.5
+            if(Dis > Safe_dist):
+                cf2.cmdPosition(cf1_pos[n],0)
+            else:
+                cf2.cmdPosition(cf2_pos,0)
+            timeHelper.sleep(0.1) #If the script does not work, check this.
+            n = n + 1
+        #cf2.notifySetpointsStop()
         
+        cf1.startTrajectory(0, timescale=TIMESCALE) # Här cf1 kör trajectory
+        cf1_pos.append(cf1.position())
+        cf2.cmdPosition(cf1_pos[9],0)
 
-        cf1.startTrajectory(0, timescale=TIMESCALE)
-        nbrloops= 10*(traj1.duration * TIMESCALE +2)
+        timeHelper.sleep(traj1.duration) # Sleep tills drönaren har kört sin runda.
+
+        allcfs.land(targetHeight=0.06, duration=4.0)
+        timeHelper.sleep(3.0)
+
+        """
+        nbrloops = 10*(traj1.duration * TIMESCALE +2)
         n = 0
         while n < nbrloops:
             cf1_pos = cf1.position()
@@ -42,9 +68,9 @@ if __name__ == "__main__":
             cf2.cmdPosition(cf2_pose_goal,0)
             timeHelper.sleep(0.1) #If the script does not work, check this.
             n = n + 1
-        
+        """
 
-        timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
+        # timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
 
         # pos_offset = cf1pos + offset
         # problem: för många pos per sekund. 
