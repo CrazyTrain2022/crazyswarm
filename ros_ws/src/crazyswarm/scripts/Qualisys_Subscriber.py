4979ca3de7cc127181d875914a93246b11b88a73
eriax970 <axelsson1999@gmail.com>
2022-11-23 16:48:06 +0100

    Added a bunch of functions to vis

--- ros_ws/src/crazyswarm/scripts/Qualisys_Subscriber.py
+++ ros_ws/src/crazyswarm/scripts/Qualisys_Subscriber.py
@@ -14,6 +14,22 @@ class Visualisation:
         self.fig = plt.figure()
         self.ax = Axes3D(self.fig)
         self.anim_running = True
+        self.save = False
+        self.cf1_log = self.ax.plot3D([], [], [], c='black', linestyle='dotted')
+        self.cf2_log = self.ax.plot3D([], [], [], c='blue', linestyle='dotted')
+        self.cf3_log = self.ax.plot3D([], [], [], c='red', linestyle='dotted')
+        self.cf4_log = self.ax.plot3D([], [], [], c='green', linestyle='dotted')
+        
+        self.cf1_pos = self.ax.scatter([], [], [], 'ko', label = "Cf1")
+        self.cf2_pos = self.ax.scatter([], [], [], 'bo', label = "Cf2")
+        self.cf3_pos = self.ax.scatter([], [], [], 'ro', label = "Cf3")
+        self.cf4_pos = self.ax.scatter([], [], [], 'go', label = "Cf4")
+
+        self.cf1_x_data, self.cf1_y_data, self.cf1_z_data = [],[],[]
+        self.cf2_x_data, self.cf2_y_data, self.cf2_z_data = [],[],[]
+        self.cf3_x_data, self.cf3_y_data, self.cf3_z_data = [],[],[]
+        self.cf4_x_data, self.cf4_y_data, self.cf4_z_data = [],[],[]
+
     def plot_init(self):
         # Setting up plot 
         self.ax.set_xlim3d(-5, 5)
@@ -27,24 +43,26 @@ class Visualisation:
         # return self.ln
 
     def Show_traj(self): # Function that plots all drone trajectories
-        traj_lst = []
+        self.traj_lst = []
+        self.traj_pos = []
         i = 0
         path = "."
         trajectory_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
         # print(trajectory_pathfiles)
         for file in trajectory_files:
             if("trajectory.csv" in file):
-                traj_lst.append(uav_trajectory.Trajectory())
-                traj_lst[i].loadcsv(file)
-                ts = np.arange(0, traj_lst[i].duration, 0.01)
+                self.traj_lst.append(uav_trajectory.Trajectory())
+                self.traj_lst[i].loadcsv(file)
+                ts = np.arange(0, self.traj_lst[i].duration, 0.01)
                 evals = np.empty((len(ts), 15))
                 for t, k in zip(ts, range(0, len(ts))):
-                    e = traj_lst[i].eval(t)
+                    e = self.traj_lst[i].eval(t)
                     evals[k, 0:3]  = e.pos
-
+                self.traj_pos.append([evals[:,0], evals[:,1], evals[:,2]]) 
                 trajectory = self.ax.plot3D(evals[:,0], evals[:,1], evals[:,2], color = 'b')
                 i += 1
-        
+        print(self.traj_pos)
+
     def Sim_Paus(self):
         if self.anim_running:
             ani.event_source.stop()
@@ -54,24 +72,6 @@ class Visualisation:
             ani.event_source.start()
             self.anim_running = True
             print("Animation resumed")
-
-class cfs:
-    def __init__(self, vis):
-        self.save = False
-        self.cf1_log = vis.ax.plot3D([], [], [], c='black', linestyle='dotted')
-        self.cf2_log = vis.ax.plot3D([], [], [], c='blue', linestyle='dotted')
-        self.cf3_log = vis.ax.plot3D([], [], [], c='red', linestyle='dotted')
-        self.cf4_log = vis.ax.plot3D([], [], [], c='green', linestyle='dotted')
-        
-        self.cf1_pos = vis.ax.scatter([], [], [], 'ko', label = "Cf1")
-        self.cf2_pos = vis.ax.scatter([], [], [], 'bo', label = "Cf2")
-        self.cf3_pos = vis.ax.scatter([], [], [], 'ro', label = "Cf3")
-        self.cf4_pos = vis.ax.scatter([], [], [], 'go', label = "Cf4")
-
-        self.cf1_x_data, self.cf1_y_data, self.cf1_z_data = [],[],[]
-        self.cf2_x_data, self.cf2_y_data, self.cf2_z_data = [],[],[]
-        self.cf3_x_data, self.cf3_y_data, self.cf3_z_data = [],[],[]
-        self.cf4_x_data, self.cf4_y_data, self.cf4_z_data = [],[],[]
 
     def cf1_callback(self, msg):
         self.cf1_x = msg.pose.position.x
@@ -88,6 +88,7 @@ class cfs:
         self.cf2_x_data.append(self.cf2_x)
         self.cf2_y_data.append(self.cf2_y) 
         self.cf2_z_data.append(self.cf2_z)
+
     def cf3_callback(self, msg):
         self.cf3_x = msg.pose.position.x
         self.cf3_y = msg.pose.position.y
@@ -116,11 +117,13 @@ class cfs:
         self.cf3_pos.set_data(self.cf3_x, self.cf3_y, self.cf3_z)
         self.cf4_pos.set_data(self.cf4_x, self.cf4_y, self.cf4_z)
     
-    def save_log(self):
+    def save_log(self, name):
+        plt.savefig(str(name) + ".png")
         np.savetxt("./Log_files/cf1_recordedPosition.csv", np.array([self.cf1_x_data, self.cf1_y_data, self.cf1_z_data]).T, delimiter = ',', fmt = '%10f')
         np.savetxt("./Log_files/cf2_recordedPosition.csv", np.array([self.cf2_x_data, self.cf2_y_data, self.cf2_z_data]).T, delimiter = ',', fmt = '%10f')
         np.savetxt("./Log_files/cf3_recordedPosition.csv", np.array([self.cf3_x_data, self.cf3_y_data, self.cf3_z_data]).T, delimiter = ',', fmt = '%10f')
         np.savetxt("./Log_files/cf4_recordedPosition.csv", np.array([self.cf4_x_data, self.cf4_y_data, self.cf4_z_data]).T, delimiter = ',', fmt = '%10f')
+
 
 if __name__ == "__main__":
     vis = Visualisation()
@@ -134,15 +137,19 @@ if __name__ == "__main__":
     start_print.pack()
     traj_print = Tkinter.Button(frame, text="Show trajectory", bg='grey', command = vis.Show_traj)
     traj_print.pack()
-    start_print = Tkinter.Button(frame, text = "Save position to file",  bg='grey', command = cfs.save_log) # variable = bool_save, onvalue=0, offvalue=1, command=save_csv)
+    save_print = Tkinter.Button(frame, text = "Save position to file",  bg='grey', command = Visualisation.save_log) # variable = bool_save, onvalue=0, offvalue=1, command=save_csv)
+    save_print.pack()
+    entry1 = Tkinter.Entry(mainwindow)
+    start_print = Tkinter.Button(frame, text = "Save plot to file",  bg='grey', command = Visualisation.save_log(entry1)) # variable = bool_save, onvalue=0, offvalue=1, command=save_csv)
     start_print.pack()
 
-    cf = cfs(vis)
-    rospy.init_node('Pose_Listener')
-    cf1 = rospy.Subscriber("/qualisys/cf1/pose", PoseStamped, cf.cf1_callback)
-    cf2 = rospy.Subscriber("/qualisys/cf2/pose", PoseStamped, cf.cf2_callback)
-    cf3 = rospy.Subscriber("/qualisys/cf3/pose", PoseStamped, cf.cf3_callback)
-    cf4 = rospy.Subscriber("/qualisys/cf4/pose", PoseStamped, cf.cf4_callback)
 
-    ani = animation.FuncAnimation(vis.fig, cf.update_plot, init_func=vis.plot_init, blit=True) # oklart om true eller ej!
+    
+    rospy.init_node('Pose_Listener')
+    cf1 = rospy.Subscriber("/qualisys/cf1/pose", PoseStamped, vis.cf1_callback)
+    cf2 = rospy.Subscriber("/qualisys/cf2/pose", PoseStamped, vis.cf2_callback)
+    cf3 = rospy.Subscriber("/qualisys/cf3/pose", PoseStamped, vis.cf3_callback)
+    cf4 = rospy.Subscriber("/qualisys/cf4/pose", PoseStamped, vis.cf4_callback)
+
+    ani = animation.FuncAnimation(vis.fig, vis.update_plot, init_func=vis.plot_init, blit=True) # oklart om true eller ej!
     plt.show()
