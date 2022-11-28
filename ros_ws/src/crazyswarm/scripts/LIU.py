640d56e4b08102a7a7756c624d057f1c20eb51d4
Victor Ryman <vicry168@student.liu.se>
2022-11-28 11:17:12 +0100

    LIU

--- ros_ws/src/crazyswarm/scripts/LIU.py
+++ ros_ws/src/crazyswarm/scripts/LIU.py
@@ -8,6 +8,15 @@ if __name__ == "__main__":
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
 
+    
+    allcfs.takeoff(targetHeight=1.0, duration=2.0)
+    timeHelper.sleep(2.5)
+    for cf in allcfs.crazyflies:
+        pos = np.array(cf.initialPosition) + np.array([0, 0, 0.5])
+        print("Position:", cf.position())
+        cf.goTo(pos, 0, 2.0)
+    timeHelper.sleep(2.5)
+
     TRIALS = 2
 
     for i in range(TRIALS):
@@ -16,43 +25,64 @@ if __name__ == "__main__":
         cf3 = swarm.allcfs.crazyflies[2]
         cf4 = swarm.allcfs.crazyflies[3]
 
-        allcfs.takeoff(targetHeight=1.0, duration=2.0)
-        timeHelper.sleep(2.5)
+        
 
-        cf1.goTo(np.array([-2,0,0.5]), 0, 2.0)
-        cf2.goTo(np.array([-1,0,0.5]), 0, 2.0)
-        cf3.goTo(np.array([0,0,0.75]), 0, 2.0)
-        cf4.goTo(np.array([1,0,0.9]), 0, 2.0)
+        cf1.goTo(np.array([-2,0,2.0]), 0, 2.0)
+        cf2.goTo(np.array([-1,0,1.5]), 0, 2.0)
+        cf3.goTo(np.array([0.25, 0, 1.0]), 0, 2.0)
+        cf4.goTo(np.array([2.17,0,1.5]), 0, 2.0)
+
+        timeHelper.sleep(3.0)
+
+        cf1.goTo(np.array([-2,0,0.5]),0, 2.0)
+        cf2.goTo(np.array([-1.0,0,0.5]),0, 2.0)
+        cf3.goTo(np.array([0.0,0.0,0.75]), 0, 2.0)
+        timeHelper.sleep(0.5)
+        cf3.goTo(np.array([0.25,0.0,0.5]), 0, 2.0)
+        cf4.goTo(np.array([1.67,0,1.5]), 0, 2.0)    
+
+        timeHelper.sleep(3.0)
 
         cf1.goTo(np.array([-1.5,0,0.5]),0, 2.0)
         cf2.goTo(np.array([-0.5,0,0.5]),0, 2.0)
-        cf3.goTo(np.array([0.25,0,0.5]),0, 2.0)
-        cf4.goTo(np.array([1.585,0,0.5]),0, 2.0)
+        cf3.goTo(np.array([0.5,0.0,0.75]), 0, 2.0)
+        timeHelper.sleep(0.5)
+        cf3.goTo(np.array([0.25,0.0,1.0]), 0, 2.0)
+        cf4.goTo(np.array([1.67,0,1.07]), 0, 2.0) 
 
-        cf1.goTo(np.array([-1.5,0,2]),0, 2.0)
-        cf2.goTo(np.array([-0.5,0,1.5]),0, 2.0)
-        cf3.goTo(np.array([0.5,0,0.75]),0, 2.0)
-        cf4.goTo(np.array([2.17,0,0.9]),0, 2.0)
-
-        cf1.goTo(np.array([-2,0,2]),0, 2.0)
-        cf2.goTo(np.array([-1,0,1.5]),0, 2.0)
-        cf3.goTo(np.array([0.25,0,1]),0, 2.0)
-        cf4.goTo(np.array([2.17,0,1.5]),0, 2.0)
-
-        cf1.goTo(np.array([-2,0,0.5]),0, 2.0)
-        cf2.goTo(np.array([-1,0,0.5]),0, 2.0)
-        cf3.goTo(np.array([0,0,0.75]),0, 2.0)
-        cf4.goTo(np.array([1.67,0,1.5]),0, 2.0)
-
-        cf4.goTo(np.array([1.67,0,1.1]),0, 2.0)
-        cf4.goTo(np.array([1.585,0,0.9]),0, 2.0)
-        cf4.goTo(np.array([1.5,0,1.1]),0, 2.0)
-        cf4.goTo(np.array([1.5,0,1.5]),0, 2.0)
-        cf4.goTo(np.array([1,0,1.5]),0, 2.0)
-        cf4.goTo(np.array([1,0,0.9]),0, 2.0)
-
-        timeHelper.sleep(1.0)
-
-
-        allcfs.land(targetHeight=0.06, duration=2.0)
         timeHelper.sleep(3.0)
+
+        cf1.goTo(np.array([-1.5,0,2.0]),0, 2.0)
+        cf2.goTo(np.array([-0.5,0,1.5]),0, 2.0)
+        cf3.goTo(np.array([0.0,0.0,0.75]), 0, 2.0)
+        cf4.goTo(np.array([1.585,0,0.9]), 0, 2.0) 
+        timeHelper.sleep(0.5)
+        cf3.goTo(np.array([0.25,0.0,0.5]), 0, 2.0)
+        cf4.goTo(np.array([1.5,0,1.07]), 0, 2.0)  
+
+        timeHelper.sleep(3.0)
+
+        cf1.goTo(np.array([-2.0,0.0,2.0]),0, 2.0)
+        cf2.goTo(np.array([-1,0,1.5]),0, 2.0)
+        cf3.goTo(np.array([0.5,0.0,0.75]), 0, 2.0)
+        timeHelper.sleep(0.5)
+        cf3.goTo(np.array([0.25,0.0,1.0]), 0, 2.0)
+        cf4.goTo(np.array([1.5,0,1.5]), 0, 2.0) 
+
+        timeHelper.sleep(2.0)
+        cf4.goTo(np.array([1,0,1.5]), 0, 2.0) 
+        timeHelper.sleep(2.0)
+        cf4.goTo(np.array([1,0,0.9]), 0, 2.0)
+        timeHelper.sleep(2.0)
+        cf4.goTo(np.array([1.585,0,0.5]), 0, 2.0)
+        timeHelper.sleep(0.50)
+        cf4.goTo(np.array([2.17,0,0.9]), 0, 2.0)
+        timeHelper.sleep(2.0)
+        cf4.goTo(np.array([2.17,0,1.5]), 0, 2.0)      
+
+
+        timeHelper.sleep(3.0)
+
+
+    allcfs.land(targetHeight=0.06, duration=2.0)
+    timeHelper.sleep(3.0)
