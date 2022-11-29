3fe937a22689b68282525ab83233498021082074
Victor Ryman <vicry168@student.liu.se>
2022-11-29 11:03:33 +0100

    LIU

--- ros_ws/src/crazyswarm/scripts/LIU.py
+++ ros_ws/src/crazyswarm/scripts/LIU.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     timeHelper.sleep(2.5)
 
 
-    TRIALS = 2
+    TRIALS = 1
 
     for i in range(TRIALS):
         
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         cf1.goTo(np.array([-2,0,2.0]), 0, 2.0)
         cf2.goTo(np.array([-1,0,1.5]), 0, 2.0)
         cf3.goTo(np.array([0.0, 0, 0.75]), 0, 2.0)
-        cf4.goTo(np.array([2.17,0,1.5]), 0, 2.0)
+        cf4.goTo(np.array([2.17,0,1.55]), 0, 2.0)
 
         timeHelper.sleep(3.0)
 
@@ -52,11 +52,13 @@ if __name__ == "__main__":
         timeHelper.sleep(0.5)
         cf3.goTo(np.array([0.5,0.0,0.75]), 0, 2.0)
         timeHelper.sleep(0.5)
-        cf3.goTo(np.array([0.25,0.0,1.0]), 0, 2.0)
+        cf3.goTo(np.array([0.25,0.0,1.2]), 0, 2.0)
+        timeHelper.sleep(0.5)
+        cf3.goTo(np.array([0.15,0.0,0.9]), 0, 2.0)
         timeHelper.sleep(0.5)
         cf3.goTo(np.array([0.0,0.0,0.75]), 0, 2.0)
 
-        cf4.goTo(np.array([1.67,0,1.5]), 0, 2.0)    
+        cf4.goTo(np.array([1.67,0,1.55]), 0, 2.0)    
 
         timeHelper.sleep(3.0)
 
@@ -80,7 +82,9 @@ if __name__ == "__main__":
         timeHelper.sleep(0.5)
         cf3.goTo(np.array([0.5,0.0,0.75]), 0, 2.0)
         timeHelper.sleep(0.5)
-        cf3.goTo(np.array([0.25,0.0,1.0]), 0, 2.0)
+        cf3.goTo(np.array([0.25,0.0,1.2]), 0, 2.0)
+        timeHelper.sleep(0.5)
+        cf3.goTo(np.array([0.15,0.0,0.9]), 0, 2.0)
         timeHelper.sleep(0.5)
         cf3.goTo(np.array([0.0,0.0,0.75]), 0, 2.0)
         cf4.goTo(np.array([1.5,0,1.5]), 0, 2.0) 
@@ -88,13 +92,15 @@ if __name__ == "__main__":
         timeHelper.sleep(2.0)
         cf4.goTo(np.array([1,0,1.5]), 0, 2.0) 
         timeHelper.sleep(2.0)
-        cf4.goTo(np.array([1,0,0.9]), 0, 2.0)
+        cf4.goTo(np.array([1,0,0.8]), 0, 2.0)
         timeHelper.sleep(2.0)
-        cf4.goTo(np.array([1.585,0,0.5]), 0, 2.0)
+        cf4.goTo(np.array([1.535,0,0.5]), 0, 2.0)
         timeHelper.sleep(0.50)
-        cf4.goTo(np.array([2.17,0,0.9]), 0, 2.0)
+        cf4.goTo(np.array([1.635,0,0.5]), 0, 2.0)
+        timeHelper.sleep(0.50)
+        cf4.goTo(np.array([2.17,0,0.8]), 0, 2.0)
         timeHelper.sleep(2.0)
-        cf4.goTo(np.array([2.17,0,1.5]), 0, 2.0)      
+        cf4.goTo(np.array([2.17,0,1.55]), 0, 2.0)      
 
 
         timeHelper.sleep(3.0)
